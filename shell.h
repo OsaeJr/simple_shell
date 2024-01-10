8d216525c0b095eee61d5ae22380bab2e7512276
OsaeJr <samueladjar11@gmail.com>
2024-01-10 09:53:47 -0800

    Edit shell.h file

--- shell.h
+++ shell.h
@@ -13,19 +13,19 @@
 
 extern char **environ;
 
-int _strcmp(char *s1, char *s2);
-size_t _strncmp(char *s1, char *s2, size_t n);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+int _strcmp_custom(char *str1, char *str2);
+unsigned int _strncmp_custom(char *str1, char *str2, unsigned int n);
+int _strlen_custom(char *str);
+char *_strcpy_custom(char *destination, char *source);
 char *_strcat_custom(char *destination, char *source)
 int _putchar(char c);
 
 char *getPath(char **environment)
 int _search_path(char **command, char **env);
 char *getUserInput(void);
-void _getenv(char **env);
-char **tokenizeInput(char *inputString)
-void _exit_command(char **args, char *lineptr, int _exit);
+void printEnvironment(char **environment);
+char **tokenizeInput(char *inputStriiing)
+void exitCommand(char **arguments, char *inputString, int exitCode);
 int executeCommand(char **commandTokens, char **programName, char **environment, char *inputLine, int processID, int checker)
 char *custom_strtok(char *str, const char *delim)
 
